5b128d606ea98066e8cc7dce6179fc7b807bbfd5
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2021-09-09 01:41:34 -0400

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Navigation from '@components/Navigation'
+import { APP_NAME } from '@config/config';
 import Head from 'next/head'
 // import { useEffect } from 'react';
 import "../styles/globals.css";
@@ -19,29 +20,29 @@ export default function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Paid to Party" />
-        <meta name="keywords" content="Paid to Party" />
-        <title>Paid to Party</title>
+        <meta name="description" content={APP_NAME} />
+        <meta name="keywords" content={APP_NAME} />
+        <title>{APP_NAME}</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
           rel="stylesheet"></link>
 
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
         <link rel="manifest" href="/manifest.json" />
         <link
-        href="/icons/icon-16x16.png"
-        rel="icon"
-        type ="image/png"
-        sizes="16x16"
+          href="/icons/icon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
         />
         <link
-        href="/icons/icon-32x32.png"
-        rel="icon"
-        type ="image/png"
-        sizes="32x32"
+          href="/icons/icon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
         />
-      {/* <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link> */}
+        {/* <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link> */}
         <meta name="theme-color" content="#dbdbdb" />
       </Head>
       <div className="relative min-h-screen flex flex-col bg-white dark:bg-black text-black pb-24" id="app">
