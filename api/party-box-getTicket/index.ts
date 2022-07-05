97ac8dc91474a4df2e5c885c5022b4f26efaf450
Conor Roberts <Conor@ConorRoberts.com>
2022-07-04 23:27:59 -0400

    Update index.ts

--- api/party-box-getTicket/index.ts
+++ api/party-box-getTicket/index.ts
@@ -1,10 +1,4 @@
-import {
-  APIGatewayEvent,
-  APIGatewayProxyEventPathParameters,
-  APIGatewayProxyEventQueryStringParameters,
-  APIGatewayProxyEventStageVariables,
-  APIGatewayProxyResultV2,
-} from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyEventPathParameters, APIGatewayProxyResultV2 } from "aws-lambda";
 import * as AWS from "@aws-sdk/client-secrets-manager";
 import { Client } from "pg";
 import stripe from "stripe";
@@ -13,13 +7,9 @@ interface PathParameters extends APIGatewayProxyEventPathParameters {
   ticketId: string;
 }
 
-interface StageVariables extends APIGatewayProxyEventStageVariables {
-  websiteUrl: string;
-}
-
 /**
  * @method POST
- * @description Retrieve ticket data from postgres and stripe given a session id and event id
+ * @description Retrieve ticket data from postgres and stripe given a session id (treated as ticketId)
  */
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResultV2<object>> => {
   console.log(event);
@@ -49,25 +39,22 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
     const { SecretString: stripeSecretString } = await secretsManager.getSecretValue({
       SecretId: "party-box/stripe",
     });
+
     if (!stripeSecretString) throw new Error("Access keys string was undefined.");
     const { secretKey: stripeSecretKey } = JSON.parse(stripeSecretString);
     const stripeClient = new stripe(stripeSecretKey, { apiVersion: "2020-08-27" });
     const session = await stripeClient.checkout.sessions.retrieve(ticketId);
+
     let paymentIntent;
     if (session.payment_intent) {
       paymentIntent = await stripeClient.paymentIntents.retrieve(session.payment_intent.toString());
     }
 
-    console.log(session);
-    await client.query(
-      `
-        select * from tickets
-        where stripe_session_id = $1;
-      `,
-      [ticketId]
-    );
-
-    return { url: session.url, session, paymentIntent };
+    return {
+      status: paymentIntent?.status ?? "pending",
+      customer_phone_number: session.customer_details?.phone,
+      customer_name: session.customer_details?.name,
+    };
   } catch (error) {
     console.error(error);
     throw error;
