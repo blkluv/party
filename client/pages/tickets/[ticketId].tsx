92f5896c1bcc6dc9e00745ee38a0f9bcf3e193a2
Conor Roberts <Conor@ConorRoberts.com>
2022-07-04 23:40:03 -0400

    added status and qr code

--- client/pages/tickets/[ticketId].tsx
+++ client/pages/tickets/[ticketId].tsx
@@ -1,25 +1,59 @@
 import { NextPageContext } from "next";
-import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import QRCode from "react-qr-code";
+import LoadingScreen from "~/components/LoadingScreen";
 import { API_URL } from "~/config/config";
 
-const Page = () => {
-  const { query } = useRouter();
-  console.log(query);
+interface Props {
+  status: "succeeded" | "failed" | "pending";
+  customer_phone_number: string;
+  customer_name: string;
+}
+
+const statusColor = {
+  succeeded: "bg-emerald-600",
+  failed: "bg-rose-600",
+  pending: "bg-gray-700",
+};
+
+const statusTranslation = {
+  pending: "Pending",
+  succeeded: "Paid",
+  failed: "Not Paid",
+};
+
+const Page = ({ status, customer_name, customer_phone_number }: Props) => {
+  const [path, setPath] = useState("");
+
+  useEffect(() => {
+    setPath(window.location.href);
+  }, []);
+
+  if (path.length === 0) return <LoadingScreen />;
+
   return (
-    <div>
-      <h1>Page</h1>
+    <div className="mx-auto max-w-2xl w-full gap-4 flex flex-col items-center">
+      <QRCode value={window.location.href} className="mx-auto" />
+      <div className={`rounded-full py-0.5 text-center px-4 ${statusColor[status]} max-w-sm`}>
+        <p>{statusTranslation[status]}</p>
+      </div>
+      <div>
+        <h1 className="font-bold text-3xl text-center">{customer_name}</h1>
+        <h2 className="font-semibold text-xl text-center">{customer_phone_number}</h2>
+      </div>
     </div>
   );
 };
 
 export const getServerSideProps = async (context: NextPageContext) => {
   const { ticketId } = context.query;
-  const response = await fetch(`${API_URL}/tickets/${ticketId}`);
+  const response = await fetch(`${API_URL}/tickets/${ticketId}`, {
+    method: "GET",
+  });
   const data = await response.json();
 
-  console.log(data);
   return {
-    props: {},
+    props: { ...data },
   };
 };
 
