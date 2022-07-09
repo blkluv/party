0c6815c581f00b85be5349897bea5f0eb590e124
Conor Roberts <Conor@ConorRoberts.com>
2022-07-08 23:09:26 -0400

    Updates

--- api/party-box-handleTicketPurchase/index.ts
+++ api/party-box-handleTicketPurchase/index.ts
@@ -1,31 +1,28 @@
-import {
-  APIGatewayEvent,
-  APIGatewayProxyEventPathParameters,
-  APIGatewayProxyEventStageVariables,
-} from "aws-lambda";
+import { APIGatewayEvent } from "aws-lambda";
 import { SecretsManager } from "@aws-sdk/client-secrets-manager";
 import stripe from "stripe";
-
-interface PathParameters extends APIGatewayProxyEventPathParameters {
-  eventId: string;
-}
-
-interface StageVariables extends APIGatewayProxyEventStageVariables {
-  websiteUrl: string;
-}
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 /**
  * @method POST
- * @description Create event within Postgres and Stripe
+ * @description Create ticket within DynamoDB and Stripe
  */
 export const handler = async (event: APIGatewayEvent): Promise<unknown> => {
   console.log(event);
   const secretsManager = new SecretsManager({});
+  const dynamo = DynamoDBDocument.from(new DynamoDB({}));
 
   try {
-    const { websiteUrl } = event.stageVariables as StageVariables;
     const { stage } = event.requestContext;
-    const { eventId } = event.pathParameters as PathParameters;
+    const {
+      data: { object: data },
+    } = JSON.parse(event.body ?? "{}");
+
+    const chargeId = data.id;
+    const customerName = data.billing_details.name;
+    const customerEmail = data.billing_details.email;
+    const receiptUrl = data.receipt_url;
 
     // Get stripe keys
     const { SecretString: stripeSecretString } = await secretsManager.getSecretValue({
@@ -34,6 +31,29 @@ export const handler = async (event: APIGatewayEvent): Promise<unknown> => {
     if (!stripeSecretString) throw new Error("Access keys string was undefined.");
     const { secretKey: stripeSecretKey } = JSON.parse(stripeSecretString);
     const stripeClient = new stripe(stripeSecretKey, { apiVersion: "2020-08-27" });
+
+    const paymentIntent = await stripeClient.paymentIntents.retrieve(data.payment_intent);
+    const session = await stripeClient.checkout.sessions.list({ payment_intent: paymentIntent.id });
+    const product = await stripeClient.products.retrieve(
+      session.data[0]?.line_items?.data[0].product?.toString() ?? ""
+    );
+
+    const eventId = product.metadata.eventId;
+    const customerPhoneNumber = session.data[0].customer_details?.phone;
+
+    await dynamo.put({
+      TableName: "party-box-tickets",
+      Item: {
+        id: session.data[0].id,
+        eventId,
+        stripeChargeId: chargeId,
+        receiptUrl,
+        customerName,
+        customerEmail,
+        customerPhoneNumber,
+        timestamp: new Date().toISOString(),
+      },
+    });
 
     return {};
   } catch (error) {
