e17529fe3e6863f6bcad7f94912658123e605c39
Conor Roberts <Conor@ConorRoberts.com>
2022-07-09 18:34:24 -0400

    references table by stage

--- api/party-box-getUpcomingEvents/index.ts
+++ api/party-box-getUpcomingEvents/index.ts
@@ -1,16 +1,18 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayEvent } from "aws-lambda";
 
 /**
  * @method POST
  * @description Create event within Postgres and Stripe
  */
-export const handler = async (): Promise<unknown> => {
+export const handler = async (event: APIGatewayEvent): Promise<unknown> => {
   try {
     const dynamo = DynamoDBDocument.from(new DynamoDB({}));
+    const { stage } = event.requestContext;
 
     const { Items: events } = await dynamo.scan({
-      TableName: "party-box-events",
+      TableName: `${stage}-party-box-events`,
       FilterExpression: "startTime > :st",
       ExpressionAttributeValues: {
         ":st": new Date().toISOString(),
