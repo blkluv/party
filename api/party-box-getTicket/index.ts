fd1c6ff95b87fb5c339108c834900644b48ddb29
Conor Roberts <Conor@ConorRoberts.com>
2022-07-09 00:33:40 -0400

    re-added status

--- api/party-box-getTicket/index.ts
+++ api/party-box-getTicket/index.ts
@@ -1,6 +1,8 @@
 import { APIGatewayEvent, APIGatewayProxyEventPathParameters } from "aws-lambda";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { SecretsManager } from "@aws-sdk/client-secrets-manager";
+import stripe from "stripe";
 
 interface PathParameters extends APIGatewayProxyEventPathParameters {
   ticketId: string;
@@ -14,9 +16,20 @@ export const handler = async (event: APIGatewayEvent): Promise<unknown> => {
   console.log(event);
 
   const dynamo = DynamoDBDocument.from(new DynamoDB({}));
+  const secretsManager = new SecretsManager({});
 
   try {
     const { ticketId } = event.pathParameters as PathParameters;
+    const { stage } = event.requestContext;
+
+    // Get stripe keys
+    const { SecretString: stripeSecretString } = await secretsManager.getSecretValue({
+      SecretId: `${stage}/party-box/stripe`,
+    });
+
+    if (!stripeSecretString) throw new Error("Access keys string was undefined.");
+    const { secretKey: stripeSecretKey } = JSON.parse(stripeSecretString);
+    const stripeClient = new stripe(stripeSecretKey, { apiVersion: "2020-08-27" });
 
     const { Item: ticketData } = await dynamo.get({
       TableName: "party-box-tickets",
@@ -34,7 +47,10 @@ export const handler = async (event: APIGatewayEvent): Promise<unknown> => {
       },
     });
 
-    return { ...ticketData, event: eventData };
+    const session = await stripeClient.checkout.sessions.retrieve(ticketId);
+    const paymentIntent = await stripeClient.paymentIntents.retrieve(session?.payment_intent?.toString() ?? "");
+
+    return { ...ticketData, status: paymentIntent?.status ?? "pending", event: eventData };
   } catch (error) {
     console.error(error);
     throw error;
