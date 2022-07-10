17e7cdd467ea3de7b6789ca5bd01e0571f2dd1c7
Conor Roberts <Conor@ConorRoberts.com>
2022-07-10 13:43:00 -0400

    Desktop formatting

--- client/pages/events/[eventId]/index.tsx
+++ client/pages/events/[eventId]/index.tsx
@@ -12,19 +12,19 @@ interface Props {
 
 const Page = ({ event }: Props) => {
   return (
-    <div className="flex flex-col mx-auto max-w-3xl w-full">
+    <div className="flex flex-col mx-auto max-w-6xl w-full md:flex-row">
       <MetaData title={`${event.name}`} />
-      <div className="relative overflow-hidden rounded-md mx-auto">
+      <div className="relative overflow-hidden rounded-md mx-auto md:flex-1">
         {event.posterUrl && <img src={event.posterUrl} alt="Poster" loading="eager" />}
       </div>
-      <div className="p-3 flex flex-col gap-6">
+      <div className="p-3 flex flex-col gap-6 md:flex-1">
         <div>
-          <h2 className="font-bold text-3xl text-center">{event.name}</h2>
-          <p className="text-sm text-center mt-1">
+          <h2 className="font-bold text-3xl md:text-4xl text-center">{event.name}</h2>
+          <p className="text-sm text-center mt-1 md:text-base">
             {new Date(event.startTime).toDateString()} at {new Date(event.startTime).toLocaleTimeString()}
           </p>
         </div>
-        <p className="text-sm">{event.description}</p>
+        <p className="text-sm md:text-base">{event.description}</p>
 
         <div className="flex justify-center">
           <Link href={event.prices[0].paymentLink} passHref>
