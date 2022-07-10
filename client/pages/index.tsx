00f2ae630af243fb678057a1efdbd8cb332f3e23
Conor Roberts <Conor@ConorRoberts.com>
2022-07-10 13:42:48 -0400

    Splash image

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
 import MetaData from "~/components/MetaData";
 import UpcomingEvents from "~/components/UpcomingEvents";
 
 const Page = () => {
   return (
     <div className="mx-auto max-w-2xl w-full">
-      <h1 className="rainbow-text text-6xl font-bold text-center my-4">Party Box</h1>
+      <div className="w-full h-72 relative mx-auto">
+        <Image
+          src="/images/Party_Box.svg"
+          layout="fill"
+          objectFit="cover"
+          alt="Orange text reading Party Box"
+          priority
+          loading="eager"
+        />
+      </div>
       <MetaData title="Home" />
       <UpcomingEvents />
     </div>
