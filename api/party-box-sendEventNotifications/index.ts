479d3d3799e86c6fee6f932289b4e7c0cc30d89e
Conor Roberts <Conor@ConorRoberts.com>
2022-07-10 12:31:24 -0400

    Updates

--- api/party-box-sendEventNotifications/index.ts
+++ api/party-box-sendEventNotifications/index.ts
@@ -1,48 +1,46 @@
-import { EventBridgeEvent, Handler } from "aws-lambda";
-import { SecretsManager } from "@aws-sdk/client-secrets-manager";
-import { Client } from "pg";
-import { SNS,OptInPhoneNumberCommand } from "@aws-sdk/client-sns";
+import { EventBridgeEvent } from "aws-lambda";
+import { SNS } from "@aws-sdk/client-sns";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+
+interface EventNotification {
+  timestamp: string;
+  eventSnsTopicArn: string;
+  message: string;
+}
 
 /**
- * @description Check if any event notifications should be sent out
+ * @description Send out event notifications to all event ticket holders.
  */
-export const handler = async (event: EventBridgeEvent) => {
+export const handler = async (event: EventBridgeEvent<"SendEventNotifications", unknown>) => {
   console.log(event);
 
-  const secretsManager = new SecretsManager({ region: "us-east-1" });
-  const sns = new SNS({ region: "us-east-1" });
+  const sns = new SNS({});
+  const dynamo = DynamoDBDocument.from(new DynamoDB({}));
 
-  // Get postgres login
-  const { SecretString } = await secretsManager.getSecretValue({ SecretId: "party-box/postgres" });
-  if (!SecretString) throw new Error("Postgres login string was undefined.");
-  const { username: user, password, host, port, dbname } = JSON.parse(SecretString);
-
-  const client = new Client({
-    user,
-    password,
-    host,
-    port,
-    database: dbname,
-  });
   try {
-    await client.connect();
+    // Get all notifications that are past due
+    const { Items: notifications } = await dynamo.scan({
+      TableName: "party-box-event-notifications",
+      FilterExpression: "timestamp <= :now",
+      ExpressionAttributeValues: {
+        ":now": new Date().toISOString(),
+      },
+    });
 
-    
-    // Subscribe a user to the notifications topic
-    await sns.send(new OptInPhoneNumberCommand({
-        phoneNumber:,
-    }))
-
-    // Send a notification
-
-    // Get all upcoming events that have not had a notification sent out
+    if (!notifications?.length) throw new Error("No notifications to send.");
 
     // Send out notifcations using AWS SNS
+    for (const e of notifications as EventNotification[]) {
+      await sns.publish({
+        Message: e.message,
+        TopicArn: e.eventSnsTopicArn,
+      });
+    }
+
     return {};
   } catch (error) {
     console.error(error);
     throw error;
-  } finally {
-    await client.end();
   }
 };
