b3ceb43f1a9e34169d09450448af602839b8c022
Conor Roberts <Conor@ConorRoberts.com>
2022-07-14 22:33:31 -0400

    created custom file upload button

---
+++ client/components/form/FileUploadField.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { OutlinedPlusIcon } from "../Icons";
+
+const FileUploadField = ({onChange}) => {
+  const [id] = useState(Math.random().toString());
+
+  return (
+    <div>
+      <input
+        onChange={(e) => onChange(e.target.files[0])}
+        type="file"
+        className="appearance-none hidden hide-file-button"
+        id={id}
+        accept="image/*"
+      />
+      <label htmlFor={id} className="border-gray-800 text-sm flex gap-2 items-center cursor-pointer hover:text-gray-200 transition rounded-md bg-gray-800 max-w-max py-1.5 px-4 ">
+        <OutlinedPlusIcon size={25} />
+        <p>Add File</p>
+      </label>
+    </div>
+  );
+};
+
+export default FileUploadField;
