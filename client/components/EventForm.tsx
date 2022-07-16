df0a76fa5a62633b055fe2e583870b25940a35a3
Conor Roberts <Conor@ConorRoberts.com>
2022-07-15 22:20:06 -0400

    Comments and creation feedback

--- client/components/EventForm.tsx
+++ client/components/EventForm.tsx
@@ -1,7 +1,9 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import axios from "axios";
 import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import deleteEvent from "~/utils/deleteEvent";
 import getToken from "~/utils/getToken";
 import { Button, CustomErrorMessage, FileUploadField, TextArea } from "./form";
 import FormGroup from "./form/FormGroup";
@@ -10,6 +12,7 @@ import { LoadingIcon } from "./Icons";
 
 const EventForm = () => {
   const { user } = useAuthenticator();
+  const router = useRouter();
 
   const [media, setMedia] = useState<File[]>([]);
   const [previewUrls, setPreviewUrls] = useState<string[]>([]);
@@ -17,6 +20,7 @@ const EventForm = () => {
   const [thumbnail, setThumbnail] = useState<File | null>(null);
   const [thumbnailPreview, setThumbnailPreview] = useState("");
 
+  // Every time media changes, generate preview URLs for all media in the array
   useEffect(() => {
     const newPreviews = media.map((e) => URL.createObjectURL(e));
     setPreviewUrls(newPreviews);
@@ -27,6 +31,8 @@ const EventForm = () => {
       });
     };
   }, [media]);
+
+  // Every time the thumbnail changes, update the preview URL
   useEffect(() => {
     if (!thumbnail) return;
     const url = URL.createObjectURL(thumbnail);
@@ -37,6 +43,7 @@ const EventForm = () => {
     };
   }, [thumbnail]);
 
+  // Add a new file to the array of media files
   const addMediaImage = (file: File) => {
     if (!file) return;
     setMedia((prev) => [...prev, file]);
@@ -49,6 +56,7 @@ const EventForm = () => {
     URL.revokeObjectURL(url);
   };
 
+  // Move the media left/right in the array
   const moveMedia = (index: number, direction: "left" | "right") => {
     if ((index === 0 && direction === "left") || (index === media.length - 1 && direction === "right")) return;
     const directionValue = direction === "left" ? -1 : 1;
@@ -69,6 +77,7 @@ const EventForm = () => {
   return (
     <Formik
       onSubmit={async (values) => {
+        let eventId = "";
         try {
           setUploadState("Creating event");
           const { data: event } = await axios.post(
@@ -77,14 +86,17 @@ const EventForm = () => {
               ...values,
               startTime: values.startTime.replace("T", " "),
               endTime: values.endTime.replace("T", " "),
-              ticketPrice: 1000,
+              ticketPrice: values.ticketPrice * 100,
             },
             { headers: { Authorization: `Bearer ${getToken(user)}` } }
           );
 
+          eventId = event.id;
+
           const posters = [];
           let mediaUploadCount = 0;
 
+          // We can only upload one file at a time.
           for (const file of media) {
             setUploadState(`Uploading media: ${++mediaUploadCount}/${media.length}`);
             const {
@@ -96,10 +108,12 @@ const EventForm = () => {
             );
             await axios.put(posterUploadUrl, file);
 
+            // Keep track of the poster download URLs so we can update the event with them later
             posters.push(posterDownloadUrl);
           }
 
           setUploadState("Uploading thumbnail");
+
           const {
             data: { uploadUrl: thumbnailUploadUrl, downloadUrl: thumbnailDownloadUrl },
           } = await axios.post(
@@ -109,26 +123,34 @@ const EventForm = () => {
           );
           await axios.put(thumbnailUploadUrl, thumbnail);
 
-          setUploadState("Uploading event");
+          setUploadState("Updating event");
+          
           await axios.post(
             `/api/events/${event.id}/update`,
             {
               media: posters,
               thumbnail: thumbnailDownloadUrl,
               ...values,
+              published: true,
             },
             { headers: { Authorization: `Bearer ${getToken(user)}` } }
           );
           setUploadState("Done");
+
+          router.push(`/events/${eventId}`);
         } catch (error) {
+          // Delete the event and its resources if we run into an error during creation
           console.error(error);
+          setUploadState("Deleting event");
+
+          await deleteEvent(eventId, getToken(user));
         }
       }}
       initialValues={{
         name: "",
         description: "",
-        startTime: "2021-01-01T00:00:00",
-        endTime: "2021-01-01T00:00:00",
+        startTime: new Date().toISOString().slice(0, 16),
+        endTime: new Date().toISOString().slice(0, 16),
         location: "",
         ticketPrice: 0,
         maxTickets: 0,
@@ -146,7 +168,7 @@ const EventForm = () => {
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <FormGroup label="Start Time" name="startTime" placeholder="Event start time" type="datetime-local" />
-            <FormGroup label="End Time (optional)" name="endTime" placeholder="Event end time" type="datetime-local" />
+            <FormGroup label="End Time" name="endTime" placeholder="Event end time" type="datetime-local" />
           </div>
           <FormGroup label="Location" name="location" placeholder="Event location (address, etc.)" />
           <FormGroup label="Ticket Price" name="ticketPrice" placeholder="Ticket price" type="number" />
@@ -168,7 +190,7 @@ const EventForm = () => {
           </div>
           <p>Thumbnail</p>
           {!thumbnail && <FileUploadField onChange={setThumbnail} />}
-          {Boolean(thumbnailPreview.length > 0) && (
+          {thumbnailPreview.length > 0 && (
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 justify-center">
               <FormPreviewImage
                 removeImage={() => {
@@ -186,7 +208,7 @@ const EventForm = () => {
             </div>
           )}
           {isSubmitting && (
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 justify-center">
               <LoadingIcon className="animate-spin" size={25} />
               <p>{uploadState}</p>
             </div>
