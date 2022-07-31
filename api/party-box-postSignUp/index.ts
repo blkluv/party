538865e987e53b8a7c55a263ae2d16642f47ed68
Conor Roberts <Conor@ConorRoberts.com>
2022-07-31 19:10:59 -0400

    Updates

--- api/party-box-postSignUp/index.ts
+++ api/party-box-postSignUp/index.ts
@@ -1,7 +1,6 @@
 import { Callback, Context, PostConfirmationTriggerEvent } from "aws-lambda";
 import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider";
-import { SecretsManager } from "@aws-sdk/client-secrets-manager";
-import knex from "knex";
+import { getPostgresClient } from "@party-box/common";
 
 /**
  * @method POST
@@ -12,55 +11,43 @@ export const handler = async (
   _context: Context,
   callback: Callback
 ): Promise<void> => {
-  const { userPoolId, userName } = event;
-
   try {
+    const { userPoolId, userName } = event;
+
     const cognito = new CognitoIdentityProvider({ region: "us-east-1" });
-    const secretsManager = new SecretsManager({});
 
-    // Get Postgres access
-    const { SecretString: pgSecretString } = await secretsManager.getSecretValue({
-      SecretId: "prod/party-box/pg",
-    });
-    if (!pgSecretString) throw new Error("Postgres secret was undefined.");
-    const pgAccess = JSON.parse(pgSecretString);
+    const pgDev = await getPostgresClient("dev");
+    const pgProd = await getPostgresClient("dev");
 
-    const pgDev = knex({
-      client: "pg",
-      connection: {
-        user: pgAccess.username,
-        password: pgAccess.password,
-        host: pgAccess.host,
-        port: pgAccess.port,
-        database: "dev",
-      },
-    });
-    // const pgProd = await getPostgresClient("prod");
+    try {
+      const userData = {
+        id: userName,
+        name: event.request.userAttributes.name,
+        email: event.request.userAttributes.email,
+        roles: ["user"],
+      };
 
-    const userData = {
-      id: userName,
-      name: event.request.userAttributes.name,
-      email: event.request.userAttributes.email,
-      roles: ["user"],
-    };
+      await pgDev("users").insert(userData);
+      console.log("User created in dev database.");
 
-    await pgDev("users").insert(userData);
+      await pgProd("users").insert(userData);
+      console.log("User created in prod database.");
 
-    console.log("User created in dev database.");
+      await cognito.adminAddUserToGroup({
+        GroupName: "user",
+        UserPoolId: userPoolId,
+        Username: userName,
+      });
 
-    await cognito.adminAddUserToGroup({
-      GroupName: "user",
-      UserPoolId: userPoolId,
-      Username: userName,
-    });
+      console.log("Group added in Cognito");
 
-    console.log("Group added in Cognito");
-
-    await pgDev.destroy();
-
-    callback(null, event);
+      return callback(null, event);
+    } finally {
+      await pgDev.destroy();
+      await pgProd.destroy();
+    }
   } catch (error) {
     console.error(error);
-    callback(error as Error, event);
+    return callback(error as Error, event);
   }
 };
