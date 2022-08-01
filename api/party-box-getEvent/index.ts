de96097840d574ef61b9dc50abe151c25b3d18f7
Conor Roberts <Conor@ConorRoberts.com>
2022-08-01 18:52:14 -0400

    Updates

--- api/party-box-getEvent/index.ts
+++ api/party-box-getEvent/index.ts
@@ -1,6 +1,5 @@
 import { APIGatewayEvent, APIGatewayProxyEventPathParameters, APIGatewayProxyResult } from "aws-lambda";
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { getPostgresClient, PartyBoxEvent } from "@party-box/common";
 
 interface PathParameters extends APIGatewayProxyEventPathParameters {
   eventId: string;
@@ -11,37 +10,27 @@ interface PathParameters extends APIGatewayProxyEventPathParameters {
  * @description Get event with given ID from DynamoDB
  */
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  const { eventId } = event.pathParameters as PathParameters;
+  const { stage } = event.requestContext;
+  const pg = await getPostgresClient(stage);
   try {
-    const { eventId } = event.pathParameters as PathParameters;
-
-    const dynamo = DynamoDBDocument.from(new DynamoDB({}));
-    const { stage } = event.requestContext;
-
-    const { Item: eventData } = await dynamo.get({
-      TableName: `${stage}-party-box-events`,
-      Key: {
-        id: eventId,
-      },
-    });
+    const [eventData] = await pg<PartyBoxEvent>("events")
+      .select("id", "startTime", "endTime", "name", "description", "hashtags", "thumbnail", "media", "prices")
+      .where("id", "=", Number(eventId));
 
     if (!eventData) throw new Error("Event not found");
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        id: eventData.id,
-        name: eventData.name,
-        description: eventData.description,
-        startTime: eventData.startTime,
-        endTime: eventData.endTime,
-        prices: eventData.prices,
-        ticketsSold: eventData.ticketsSold,
-        media: eventData.media,
-        thumbnail: eventData.thumbnail,
-      }),
+      body: JSON.stringify(eventData),
     };
   } catch (error) {
     console.error(error);
-    throw error;
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  } finally {
+    await pg.destroy();
   }
 };
