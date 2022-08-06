64343f63201f474270ea869e523bef1905d9d68c
Conor Roberts <Conor@ConorRoberts.com>
2022-08-06 15:50:36 -0400

    updatess

--- api/party-box-sendEventNotifications/index.ts
+++ api/party-box-sendEventNotifications/index.ts
@@ -1,55 +1,51 @@
-import { EventBridgeEvent } from "aws-lambda";
 import { SNS } from "@aws-sdk/client-sns";
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-
-interface EventNotification {
-  messageTime: string;
-  eventSnsTopicArn: string;
-  message: string;
-  id: string;
-}
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { getPostgresClient, PartyBoxEvent, PartyBoxEventNotification } from "@party-box/common";
 
 /**
  * @description Send out event notifications to all event ticket holders.
  */
-export const handler = async (event: EventBridgeEvent<"SendEventNotifications", unknown>) => {
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   console.log(event);
 
   const sns = new SNS({});
-  const dynamo = DynamoDBDocument.from(new DynamoDB({}));
+  const { stage } = event.requestContext;
+
+  const pg = await getPostgresClient(stage);
 
   try {
-    // Get all notifications that are past due
-    const { Items: notifications } = await dynamo.scan({
-      TableName: "party-box-event-notifications",
-      FilterExpression: "messageTime <= :now",
-      ExpressionAttributeValues: {
-        ":now": new Date().toISOString(),
-      },
-    });
+    const notifications = await pg<PartyBoxEventNotification>("eventNotifications")
+      .select("*")
+      .where("messageTime", "<=", "now()")
+      .join<PartyBoxEvent>("events", "events.id", "=", "eventNotifications.eventId");
 
-    if (!notifications?.length) throw new Error("No notifications to send.");
+    if (notifications?.length === 0) throw new Error("No notifications to send.");
 
     // Send out notifcations using AWS SNS
-    for (const e of notifications as EventNotification[]) {
+    for (const e of notifications) {
       await sns.publish({
         Message: e.message,
-        TopicArn: e.eventSnsTopicArn,
-      });
-
-      // Delete record from DynamoDB
-      await dynamo.delete({
-        TableName: "party-box-event-notifications",
-        Key: {
-          id: e.id,
-        },
+        TopicArn: e.snsTopicArn,
       });
     }
 
-    return {};
+    // Delete all selected messages
+    await pg<PartyBoxEventNotification>("eventNotifications").whereIn(
+      "id",
+      notifications.map((e) => e.id)
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ status: "Success" }),
+    };
   } catch (error) {
     console.error(error);
-    throw error;
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ status: "No notifications to send" }),
+    };
+  } finally {
+    await pg.destroy();
   }
 };
