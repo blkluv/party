31356c8a449aee89c7954a8515bae9eb24e9c8ca
Conor Roberts <Conor@ConorRoberts.com>
2022-08-06 12:10:33 -0400

    updates

--- api/party-box-getUpcomingEvents/index.ts
+++ api/party-box-getUpcomingEvents/index.ts
@@ -1,5 +1,7 @@
-import { getPostgresClient, PartyBoxEvent } from "@party-box/common";
+// import { getPostgresClient, PartyBoxEvent } from "@party-box/common";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { SecretsManager } from "@aws-sdk/client-secrets-manager";
+import { Client } from "pg";
 
 /**
  * @method POST
@@ -7,12 +9,36 @@ import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
  */
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   const { stage } = event.requestContext;
-  const pg = await getPostgresClient(stage);
+  const secretsManager = new SecretsManager({});
+  // Get Postgres access
+  const { SecretString: pgSecretString } = await secretsManager.getSecretValue({
+    SecretId: "prod/party-box/pg",
+  });
+  if (!pgSecretString) throw new Error("Postgres secret was undefined.");
+  const pgAccess = JSON.parse(pgSecretString);
+
+  const client = new Client({
+    user: pgAccess.username,
+    password: pgAccess.password,
+    host: pgAccess.host,
+    port: pgAccess.port,
+    database: stage,
+  });
+
+  await client.connect();
+
   try {
-    const events = await pg<PartyBoxEvent>("events")
-      .select("id", "startTime", "endTime", "name", "description", "hashtags", "maxTickets", "thumbnail")
-      .where("published", "=", true)
-      .andWhere("startTime", ">", new Date().toISOString());
+    // const events = await pg<PartyBoxEvent>("events")
+    //   .select("id", "startTime", "endTime", "name", "description", "hashtags", "maxTickets", "thumbnail")
+    //   .where("published", "=", true)
+    //   .andWhere("startTime", ">", new Date().toISOString());
+
+    const events = await client.query(`
+        SELECT id, startTime, endTime, name, description, hashtags, maxTickets, thumbnail
+        FROM events
+        WHERE published = true
+        AND startTime > NOW()
+    `);
 
     return { statusCode: 200, body: JSON.stringify(events) };
   } catch (error) {
@@ -20,6 +46,6 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
 
     return { statusCode: 500, body: JSON.stringify(error) };
   } finally {
-    await pg.destroy();
+    await client.end();
   }
 };
