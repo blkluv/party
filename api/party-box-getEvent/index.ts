b591f6b07ccea2ad11c4eb1019eb4b0c756fcdc7
Conor Roberts <Conor@ConorRoberts.com>
2022-08-18 18:41:30 -0400

    now includes host data

--- api/party-box-getEvent/index.ts
+++ api/party-box-getEvent/index.ts
@@ -1,5 +1,5 @@
 import { APIGatewayEvent, APIGatewayProxyEventPathParameters, APIGatewayProxyResult } from "aws-lambda";
-import { getPostgresClient, PartyBoxEvent } from "@party-box/common";
+import { getPostgresClient, PartyBoxEvent, PartyBoxHost } from "@party-box/common";
 
 interface PathParameters extends APIGatewayProxyEventPathParameters {
   eventId: string;
@@ -12,17 +12,23 @@ interface PathParameters extends APIGatewayProxyEventPathParameters {
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   const { eventId } = event.pathParameters as PathParameters;
   const { stage } = event.requestContext;
+
   const pg = await getPostgresClient(stage);
+
   try {
     const [eventData] = await pg<PartyBoxEvent>("events")
-      .select("id", "startTime", "endTime", "name", "description", "hashtags", "thumbnail", "media", "prices")
+      .select("id", "startTime", "endTime", "name", "description", "hashtags", "thumbnail", "media", "prices", "hostId")
       .where("id", "=", Number(eventId));
 
     if (!eventData) throw new Error("Event not found");
 
+    const [hostData] = await pg<PartyBoxHost>("hosts").select("name", "imageUrl", "id", "description").where("id", "=", Number(eventData.hostId));
+
+    if (!hostData) throw new Error("Host not found");
+
     return {
       statusCode: 200,
-      body: JSON.stringify(eventData),
+      body: JSON.stringify({ ...eventData, host: hostData }),
     };
   } catch (error) {
     console.error(error);
