75be4830da5a2e47871cac59e49a2bc14274a6db
Conor Roberts <Conor@ConorRoberts.com>
2022-08-18 23:21:17 -0400

    Now verifies role as well

--- api/party-box-updateHost/index.ts
+++ api/party-box-updateHost/index.ts
@@ -1,37 +1,45 @@
-import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyEventPathParameters, APIGatewayProxyResult } from "aws-lambda";
 import { getPostgresClient, decodeJwt, PartyBoxHost, PartyBoxHostRole } from "@party-box/common";
 
+interface PathParameters extends APIGatewayProxyEventPathParameters {
+  hostId: string;
+}
+
 /**
- * Create a host entity within Postgres
+ * Update a host entity within Postgres
  */
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   console.log(JSON.stringify(event));
 
   const { stage } = event.requestContext;
   const { Authorization } = event.headers;
+  const { hostId } = event.pathParameters as PathParameters;
 
   const pg = await getPostgresClient(stage);
 
   try {
     if (!event.body) throw new Error("Missing event body");
 
-    const { name, description } = JSON.parse(event.body);
+    const { name, description, imageUrl } = JSON.parse(event.body);
 
     const { sub: userId } = decodeJwt(Authorization, ["admin", "user"]);
+
+    // Check whether the user is an admin/manager of the host.
+    const hostRole = await pg<PartyBoxHostRole>("hostRoles")
+      .select("role")
+      .where([["hostId", "=", hostId], ["userId", "=", userId]]).first();
+
+    if (!hostRole) throw Error("Couldn't find host role record.");
+
+    if (hostRole.role !== "admin" && hostRole.role !== "manager") throw new Error("User is not permitted to update this host.");
 
     const [newHostData] = await pg<PartyBoxHost>("hosts")
       .insert({
         name,
         description,
-        createdBy: userId,
+        imageUrl
       })
       .returning("*");
-
-    await pg<PartyBoxHostRole>("hostRoles").insert({
-      hostId: newHostData.id,
-      userId,
-      role: "admin",
-    });
 
     return {
       statusCode: 200,
