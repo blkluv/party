6a5424ab291046793c41d7fd16868be89b3db034
Conor Roberts <Conor@ConorRoberts.com>
2022-08-18 23:52:24 -0400

    updated to include full workflow

--- client/utils/createHost.ts
+++ client/utils/createHost.ts
@@ -1,10 +1,29 @@
-import { PartyBoxHost,PartyBoxCreateHostInput } from "@party-box/common";
+import { PartyBoxHost, PartyBoxCreateHostInput } from "@party-box/common";
 import axios from "axios";
 
-const createHost = async (newHostData: PartyBoxCreateHostInput, token: string) => {
+const createHost = async (newHostData: PartyBoxCreateHostInput, image: File, token: string) => {
   try {
-    const { data } = await axios.post<PartyBoxHost>("/api/host/create", newHostData, { headers: { Authorization: `Bearer ${token}` } });
-    return data;
+    const { data: createdHost } = await axios.post<PartyBoxHost>("/api/host/create", newHostData, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const {
+      data: { uploadUrl, downloadUrl },
+    } = await axios.post<{ downloadUrl: string; uploadUrl: string }>(
+      `/api/host/${createdHost.id}/upload`,
+      { name: image.name },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    await axios.put(uploadUrl, image);
+
+    const { data: updatedHost } = await axios.post<PartyBoxHost>(
+      `/api/host/${createdHost.id}`,
+      { imageUrl: downloadUrl },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    return updatedHost;
   } catch (error) {
     console.error(error);
     throw error;
