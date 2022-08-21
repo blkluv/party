262e80cafd23d9e8214760ba2ccdbb068ac249f3
Conor Roberts <Conor@ConorRoberts.com>
2022-08-20 21:09:47 -0400

    Update index.ts

--- api/party-box-getHostRoles/index.ts
+++ api/party-box-getHostRoles/index.ts
@@ -18,12 +18,12 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
 
   try {
     // Verify that the requesting user has permission to view this host
-    const { sub: userId } = decodeJwt(Authorization, ["admin", "host"]);
+    const { sub: userId } = decodeJwt(Authorization, ["host"]);
 
     if (!userId) throw new Error("Missing userId");
 
     const validRole = await verifyHostRoles(pg, userId, Number(hostId), ["admin", "manager"]);
-    
+
     if (!validRole) throw new Error("User does not have permission to get roles for this host");
 
     const hostUsers = await pg<PartyBoxHostRole>("hostRoles")
