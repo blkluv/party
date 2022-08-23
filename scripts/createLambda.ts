4c78220b9408c94fc7bf9d8386603a6b21938c8e
Conor Roberts <Conor@ConorRoberts.com>
2022-08-22 22:17:22 -0400

    WIP - Creating AWS resources

--- scripts/createLambda.ts
+++ scripts/createLambda.ts
@@ -1,10 +1,16 @@
 import fs from "fs";
+import { LambdaClient, CreateFunctionCommand } from "@aws-sdk/client-lambda";
+import consola from "consola";
+import ora from "ora";
+import { exit } from "process";
+
+const lambda = new LambdaClient({});
 
 const [workflowName] = process.argv.slice(2);
 
 if (!workflowName) {
   console.error("Workflow name is required");
-  process.exit(1);
+  exit(1);
 }
 
 const workflowFolderPath = "../.github/workflows";
@@ -18,13 +24,18 @@ const newProdWorkflow = `../.github/workflows/prod-party-box-${workflowName}.yml
 const devWorkflowText = fs.readFileSync(`${workflowFolderPath}/dev-party-box-${templateWorkflowName}.yml`, "utf8");
 const prodWorkflowText = fs.readFileSync(`${workflowFolderPath}/prod-party-box-${templateWorkflowName}.yml`, "utf8");
 
+consola.success("Existing workflows read");
+
 const newDevText = devWorkflowText.replace(new RegExp(`${templateWorkflowName}`, "g"), workflowName);
 const newProdText = prodWorkflowText.replace(new RegExp(`${templateWorkflowName}`, "g"), workflowName);
 
 fs.writeFileSync(newDevWorkflow, newDevText);
 fs.writeFileSync(newProdWorkflow, newProdText);
 
+consola.success("New workflows created");
+
 fs.mkdirSync(`${functionFolderPath}/party-box-${workflowName}`);
+consola.success("New API folder created");
 
 fs.readdirSync(`${functionFolderPath}/party-box-${templateFunctionName}`).forEach((fileName) => {
   const newFilePath = `${functionFolderPath}/party-box-${workflowName}/${fileName}`;
@@ -34,3 +45,21 @@ fs.readdirSync(`${functionFolderPath}/party-box-${templateFunctionName}`).forEac
 
   fs.copyFileSync(oldFilePath, newFilePath);
 });
+
+consola.success("Files copied to new folder");
+
+exit();
+
+(async () => {
+  const spinner = ora("Creating Lambda function").start();
+  const createFunctionCommand = new CreateFunctionCommand({
+    Code: {
+      ImageUri: "public.ecr.aws/lambda/nodejs:12.2022.08.22.13",
+    },
+    FunctionName: `party-box-${workflowName}`,
+    Role: "",
+  });
+  spinner.stop();
+
+  await lambda.send(createFunctionCommand);
+})();
