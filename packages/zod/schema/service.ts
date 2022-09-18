0ec059b566a411b80344790ac4e314b15aadec4d
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2022-09-18 18:49:23 -0400

    Unusewd

--- packages/zod/schema/service.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod"
-
-export const ServiceModel = z.object({
-  id: z.number().int(),
-  name: z.string(),
-  description: z.string().nullish(),
-  imageUrl: z.string().nullish(),
-  price: z.number(),
-})
