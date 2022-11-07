98ee7eb315fcbcfe1b64df113229289eaa374e59
Conor Roberts <Conor@ConorRoberts.com>
2022-11-07 11:40:42 -0500

    new testimonial

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -24,11 +24,6 @@ const Page = () => {
         <h2 className="font-bold text-xl mb-6 text-center">Testimonials</h2>
         <div className="flex flex-wrap gap-8 justify-center">
           <Testimonial
-            imageUrl="/images/testimony_1.jpg"
-            name="David, 3rd Year, UofG"
-            description={`"I used Party Box to host my best friends party last sem. Got a DJ, Speakers, Party Lights they even blessed a photographer. Would definitely recommend it if you're looking to party and make money at the same time."`}
-          />
-          <Testimonial
             imageUrl="/images/testimony_2.png"
             name="Chloe, Grad, UofG"
             description={`"I needed rent so I Party Boxed my house for an Hawaiian themed party. Great Vibes 10/10 for sure would recommend."`}
@@ -37,6 +32,11 @@ const Page = () => {
             imageUrl="/images/testimony_3.png"
             name="Duncan, 4th Year, UofG"
             description={`"Hosted a toga party with Party Box last year. Had a great time , made some cash and still had a functioning toilet after all of it. Looking forward to hosting more next semester."`}
+          />
+          <Testimonial
+            imageUrl="/images/testimony_4.jpg"
+            name="Matt, 3rd Year, UofG"
+            description={`"Shoutout to Party Box and Spidr for my Halloween party. I'll always be able to host big bangers that attract hundreds."`}
           />
         </div>
       </div>
