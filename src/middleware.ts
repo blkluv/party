bca1968734276d43a7e7d78a6a58f10ce60593a7
Conor Roberts <Conor@ConorRoberts.com>
2023-07-30 15:14:00 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/", "/events/:eventSlug", "/api/(.*)"],
+  afterAuth: () => {
+    return NextResponse.json({}, { status: 200 });
+  },
 });
 
 export const config = {
