2730e4013c00017833ad828c593a9fd9cb017499
Conor Roberts <Conor@ConorRoberts.com>
2023-07-30 13:51:30 -0400

    updates

--- src/app/_components/trpc-provider.tsx
+++ src/app/_components/trpc-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { getFetch, httpBatchLink, loggerLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { useMemo, useState } from "react";
 import superjson from "superjson";
 import { env } from "~/config/env";
@@ -19,11 +19,7 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
   );
 
   // Use Vercel URL if present
-  const url = `${
-    process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : env.NEXT_PUBLIC_WEBSITE_URL
-  }/api/trpc`;
+  const url = `${env.NEXT_PUBLIC_WEBSITE_URL}/api/trpc`;
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -34,9 +30,9 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
         httpBatchLink({
           url,
           fetch: async (input, init?) => {
-            const fetch = getFetch();
             return fetch(input, {
               ...init,
+              cache: "no-store",
               credentials: "include",
             });
           },
