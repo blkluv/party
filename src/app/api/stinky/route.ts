4b5be4cf745ad6abcb69105a1163b96e542b3458
Conor Roberts <Conor@ConorRoberts.com>
2023-07-30 14:56:20 -0400

    Update route.ts

--- src/app/api/stinky/route.ts
+++ src/app/api/stinky/route.ts
@@ -6,7 +6,7 @@ export const GET = () => {
     {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "https://partybox.im",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
