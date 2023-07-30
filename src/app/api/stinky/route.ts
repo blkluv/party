62301a642137c8c8e06b9331f00cfc2a55c777a3
Conor Roberts <Conor@ConorRoberts.com>
2023-07-30 14:52:36 -0400

    test

--- src/app/api/stinky/route.ts
+++ src/app/api/stinky/route.ts
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
 
 export const GET = () => {
-  return NextResponse.json({}, { status: 200 });
+  return NextResponse.json(
+    {},
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    }
+  );
 };
