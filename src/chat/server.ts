6bdf42d953f03406ddeba6af81619c9c4234fd9e
Conor Roberts <Conor@ConorRoberts.com>
2023-08-04 10:34:08 -0400

    CI/CD

--- src/chat/server.ts
+++ src/chat/server.ts
@@ -2,7 +2,7 @@ import { createClient } from "@libsql/client/web";
 import { createId } from "@paralleldrive/cuid2";
 import { asc, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/libsql";
-import type { PartyKitServer } from "partykit/server";
+import type { PartyKitRoom, PartyKitServer } from "partykit/server";
 import { z } from "zod";
 import type { InitialMessagesEvent } from "~/utils/chat";
 import { socketEventSchema } from "~/utils/chat";
@@ -14,22 +14,28 @@ const getDb = (args: { url: string; authToken: string }) => {
   return drizzle(libsqlClient, { schema });
 };
 
-const getEnv = (envRecord: Record<string, unknown>) => {
+const getEnv = (room: PartyKitRoom) => {
   const env = z
     .object({
       OPENAI_API_KEY: z.string(),
       DATABASE_URL: z.string(),
       DATABASE_AUTH_TOKEN: z.string(),
     })
-    .safeParse(envRecord);
+    .safeParse({
+      OPENAI_API_KEY: room.env.OPENAI_API_KEY,
+      DATABASE_URL: room.env.DATABASE_URL,
+      DATABASE_AUTH_TOKEN: room.env.DATABASE_AUTH_TOKEN,
+    });
 
   return env;
 };
 
 const server: PartyKitServer = {
   onConnect: async (ws, room) => {
-    const env = getEnv(room.env);
+    const env = getEnv(room);
+
     if (!env.success) {
+      console.log(env.error.message);
       return;
     }
 
@@ -53,24 +59,22 @@ const server: PartyKitServer = {
     ws.send(JSON.stringify(event));
   },
   onMessage: async (event, ws, room) => {
-    const env = z
-      .object({
-        OPENAI_API_KEY: z.string(),
-        DATABASE_URL: z.string(),
-        DATABASE_AUTH_TOKEN: z.string(),
-      })
-      .safeParse(room.env);
+    const env = getEnv(room);
 
     if (!env.success) {
+      console.log(env.error.message);
       return;
     }
 
     if (typeof event === "string") {
+      console.log(event);
       const validatedMessage = socketEventSchema.safeParse(JSON.parse(event));
 
       if (!validatedMessage.success) {
+        console.log(validatedMessage.error.message);
         return;
       } else if (validatedMessage.data.__type === "CHAT_MESSAGE") {
+        console.log(validatedMessage.data.data);
         const db = getDb({
           authToken: env.data.DATABASE_AUTH_TOKEN,
           url: env.data.DATABASE_URL,
