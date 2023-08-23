85e2e5d32f6e56b703dcc4d97adcc7f5535d0f1b
Conor Roberts <Conor@ConorRoberts.com>
2023-08-23 15:04:04 -0400

    Update page.tsx

--- src/app/events/[eventId]/promotion-codes/page.tsx
+++ src/app/events/[eventId]/promotion-codes/page.tsx
@@ -1,10 +1,11 @@
 import { auth } from "@clerk/nextjs";
-import { and, eq, inArray } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { getDb } from "~/db/client";
-import { eventRoles, events } from "~/db/schema";
+import { events } from "~/db/schema";
 import { getPageTitle } from "~/utils/getPageTitle";
+import { getUserEventRole } from "~/utils/getUserEventRole";
 import { ManagePromotionCodes } from "../manage/event-management-helpers";
 
 type PageProps = { params: { eventId: string } };
@@ -25,33 +26,21 @@ const Page = async (props: PageProps) => {
 
   const event = await db.query.events.findFirst({
     where: and(eq(events.id, props.params.eventId)),
-    with: {
-      roles: {
-        where: and(
-          eq(eventRoles.userId, userAuth.userId),
-          inArray(eventRoles.role, ["admin", "manager"])
-        ),
-      },
-    },
   });
 
   if (!event) {
     redirect(`/events/${props.params.eventId}`);
   }
 
-  const userEventRole =
-    event.userId === userAuth.userId ? "admin" : event?.roles[0]?.role;
+  const userEventRole = await getUserEventRole(props.params.eventId);
 
-  if (!userEventRole) {
+  if (userEventRole !== "admin") {
     redirect(`/events/${props.params.eventId}`);
   }
 
   return (
     <div className="max-w-2xl w-full my-8 px-2 mx-auto">
-      <ManagePromotionCodes
-        eventId={props.params.eventId}
-        // role={userEventRole}
-      />
+      <ManagePromotionCodes eventId={props.params.eventId} />
     </div>
   );
 };
